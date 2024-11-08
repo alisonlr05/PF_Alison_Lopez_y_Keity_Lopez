b21698acbc2a22144bd6a7d8ce87917ac57a7a86
alisonlr05 <ALISON.LOPEZREYES@ucr.ac.cr>
2024-11-08 16:11:22 -0600

    confirmar

---
+++ POO/ConexionDB.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/gestion_cargos";
+    private static final String USER = "root";
+    private static final String PASSWORD = "alison2710";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+            return null;
+        }
+    }
+}
